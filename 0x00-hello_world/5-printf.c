a34d1aae1a7f3af3373c5b0936332c150e2a670f
MAHMOUD128 <mahmoudkhaleel551@gmail.com>
2023-03-16 14:21:52 -0700

    repet 3

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * main - here we are using printf function
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
