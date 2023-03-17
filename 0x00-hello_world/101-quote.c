37ac73a385d609798dd50d8380e57e97a81b0b9f
MAHMOUD128 <mahmoudkhaleel551@gmail.com>
2023-03-16 19:29:12 -0700

    last

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+  * main - program to print sentence on terminal with using unix simple operations
+  * Return: (1) Success
  */
 int main(void)
 {
-	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"; 
 	write(1, str, sizeof(str)-1);
 	return (1);
 }
