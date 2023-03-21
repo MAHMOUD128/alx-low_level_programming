f3edb222317e48a38fe080d3eb9ecf049e9689e5
MAHMOUD128 <mahmoudkhaleel551@gmail.com>
2023-03-21 14:48:46 -0700

    task2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,18 +3,16 @@
 /**
  * print_alphabet - Print a-z in one line
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (Succes)
  */
 
 void print_alphabet(void)
 {
-
 	int i;
 
-    	for (i = 97 ; i < 123 ; i++)
-    	{
-        	_putchar(i);
-    	}
-    	_putchar('\n');
-
+	for(i = 97 ; i < 123 ; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
 }
