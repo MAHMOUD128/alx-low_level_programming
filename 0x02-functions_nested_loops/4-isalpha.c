e41b37532847670b27fb4a6fb1f1034f750b8e66
MAHMOUD128 <mahmoudkhaleel551@gmail.com>
2023-03-21 15:34:14 -0700

    task4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isalpha - return 1 if lowercase or uppercase and 0 for else.
+ * @c: the char in ASCII code
+ *
+ * Return: 1 for lowercase or uppercase and 0 for else
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <=90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
