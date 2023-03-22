b25ef8da12488fd28fbd278b2600e7f948bbc928
MAHMOUD128 <mahmoudkhaleel551@gmail.com>
2023-03-21 19:23:26 -0700

    task11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * print_to_98 - to print from n to 98 or oppisite.
+ * @n: end or start loop.
+ * Return: no return.
+ */
+
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		int i;
+
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d" , i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	else if (n < 98)
+	{
+		int j;
+
+		for (j = n; j <= 98; j++)
+		{
+			printf("%d" , j);
+			if (j != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	else
+	{
+		printf("%d" , n);
+	}
+}
