731ea1dcb8721c055db530644af8ae2171a41079
MAHMOUD128 <mahmoudkhaleel551@gmail.com>
2023-03-28 16:42:14 -0700

    test task 3

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,16 +4,16 @@
  * _strlen - returns the length of a string
  * @s: string to be measured
  * Return: length of string
- * */
+ */
 
 int _strlen(char *s)
 {
 	int i = 0;
 
-    	while (s[i] != '\0')
-    	{
-        	i++;
+	while (s[i] != '\0')
+	{
+    		i++;
     	}
 
-    	return (i);
+	return (i);
 }
