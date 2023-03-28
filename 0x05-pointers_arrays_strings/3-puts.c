b96b82b9b2006f076f862af93fd16c148ffd88db
MAHMOUD128 <mahmoudkhaleel551@gmail.com>
2023-03-28 16:50:36 -0700

    test task 4

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -4,16 +4,14 @@
  * _puts - prints a string, followed by a new line, to stdout.
  * @str: string to print
  */
-
 void _puts(char *str)
 {
-    int i = 0;
+	int i;
 
-    while (str[i] != '\0')
-    {
-        _putchar(str[i]);
-        i++;
-    }
-    _putchar('\n');
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
 
+	_putchar('\n');
 }
